ce74a8be381a84d287c78afa42ff7e0c5b06dca9
tanvir <tanvir.ali@iprospect.com>
2021-02-02 07:04:56 +0000

    adding curr score back in for reseting styles

--- src/ts/components/reset/resetStyles.ts
+++ src/ts/components/reset/resetStyles.ts
@@ -5,13 +5,19 @@ const hold = document.querySelector('.hold') as HTMLElement;
 const player1 = document.querySelector('.left') as HTMLElement;
 const player2 = document.querySelector('.right') as HTMLElement;
 
+const currScores: any = document.getElementsByClassName('currentScore');
+
 export const resetStyles = () => {
+	for (const currScore of currScores) {
+		currScore.style.display = 'flex';
+	}
+
 	dice.style.display = 'none';
 	roll.style.display = 'block';
 	hold.style.display = 'block';
 
-	player1.classList.remove('bg-opacity-30');
-	player2.classList.remove('active', 'bg-opacity-60');
+	player1.classList.remove('bg-opacity-30', 'bg-red-500');
+	player2.classList.remove('active', 'bg-opacity-60', 'bg-red-500');
 	player1.classList.add('active', 'bg-white', 'bg-opacity-60');
 	player2.classList.add('bg-white', 'bg-opacity-30');
 
